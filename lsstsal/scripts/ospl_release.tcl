ade8942f19094742d8ed042fce40911b200668c2
Dave Mills <rfactory@theriver.com>
2020-06-05 16:30:21 -0700

    Fix OSPL release interrogator

--- lsstsal/scripts/ospl_release.tcl
+++ lsstsal/scripts/ospl_release.tcl
@@ -1,7 +1,6 @@
 #!/usr/bin/env tclsh
 
-set whichidl [exec which idlpp]
-set vers [split [exec strings $whichidl | grep OSPL_V] "/_"]
+set vers [split [exec strings $env(OSPL_HOME)/bin/idlpp | grep OSPL_V] "/_"]
 set nfld [lsearch $vers OSPL]
 set OSPL_RELEASE [string range [join [lrange $vers [expr $nfld+1] [expr $nfld+3]] "."] 1 end]
 puts stdout $OSPL_RELEASE
