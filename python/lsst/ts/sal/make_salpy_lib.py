a27c5bef404ad5bbaee439af830c0538113aacd0
Tiago Ribeiro <tiago.astro@gmail.com>
2021-09-13 13:39:28 -0700

    Remove work around to components with no xml definition in `make_idl_file.py` and `make_salpy_lib.py`.

--- python/lsst/ts/sal/make_salpy_lib.py
+++ python/lsst/ts/sal/make_salpy_lib.py
@@ -79,21 +79,6 @@ class MakeSalpyLib:
             for n in ("SALGenerics", "SALSubsystems")
         ]
         sal_xml_paths = glob.glob(os.path.join(interfaces_dir, self.sal_name, "*.xml"))
-        if not sal_xml_paths:
-            print(f"Note: SAL component {self.name} only uses generic topics")
-
-            dummy_xml_event_file = os.path.join(
-                self.sal_work_dir, f"{self.sal_name}_Events.xml"
-            )
-
-            with open(dummy_xml_event_file, "w") as fp:
-                fp.write(
-                    """<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="http://lsst-sal.tuc.noao.edu/schema/SALEventSet.xsl"?>
-<SALEventSet xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://lsst-sal.tuc.noao.edu/schema/SALEventSet.xsd">
-</SALEventSet>
-"""
-                )
         for xmlpath in std_xml_paths + sal_xml_paths:
             shutil.copy(xmlpath, self.sal_work_dir)
 
