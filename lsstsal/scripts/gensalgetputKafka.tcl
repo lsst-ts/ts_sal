b79f20c93f606cd12f509d2380e4e8a37caa539d
Dave Mills <dmills@lsst.org>
2024-02-13 13:25:41 -0700

    Fix ackcmd schema version, command history, and m1m3 build mods

--- lsstsal/scripts/gensalgetputKafka.tcl
+++ lsstsal/scripts/gensalgetputKafka.tcl
@@ -349,6 +349,9 @@ global SAL_WORK_DIR ACTIVETOPICS AVRO_PREFIX
       puts $fout "    strcpy(sal\[$idx\].topicHandle,\"[set base]_[set name][set revcode]\");"
       puts $fout "    strcpy(sal\[$idx\].topicName,\"[set base]_[set name]\");"
       puts $fout "    sal\[$idx\].avroName = \"[getAvroNamespace][set base].[set name]\";"
+      if { $type == "command" } {
+        puts $fout "   sal\[$idx\].historyDepth=0;"
+      }
       if { $type == "logevent" } {
         puts $fout "   sal\[$idx\].historyDepth=1;"
       }
@@ -856,11 +859,14 @@ global env SAL_DIR SAL_WORK_DIR SYSDIC TLMS EVTS OPTIONS ACTIVETOPICS
        if { [file exists $SAL_WORK_DIR/SAL_[set base]_enums.h] } {
           set fein  [open  $SAL_WORK_DIR/SAL_[set base]_enums.h r]
           set feout [open $SAL_WORK_DIR/[set base]/cpp/src/SAL_[set base]_enums.h w]
+          puts $feout "#ifndef AVRO_[set base]_ENUMS"
+          puts $feout "#define AVRO_[set base]_ENUMS"
           puts $feout "namespace [set base] \{"
           while { [gets $fein rec] > -1 } {
             puts $feout $rec
           }
           puts $feout "\}"
+          puts $feout "#endif"
           close $fein
           close $feout
           puts $fouth "  #include \"SAL_[set base]_enums.h\""
